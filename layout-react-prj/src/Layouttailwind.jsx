e1c616925dff48a15998dad7cb8f8dffbee2b909
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-06 18:08:57 +0330

    create jsx file

---
+++ layout-react-prj/src/Layouttailwind.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Layouttailwind() {
+  return (
+    <div className='flex flex-col justify-center items-center '>
+      <h1 className='text-3xl text-blue-500 ml-10 my-10'>Hi there !</h1>
+      <button className='border-2 border-red-400 '>About Me</button>
+    </div>
+  )
+}
+
+export default Layouttailwind
