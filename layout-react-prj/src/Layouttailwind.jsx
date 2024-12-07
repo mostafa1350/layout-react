be4a85aba0c410089fe11cf180bf5a9c726e3af0
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-07 23:41:40 +0330

    background image dark mode testing

--- layout-react-prj/src/Layouttailwind.jsx
+++ layout-react-prj/src/Layouttailwind.jsx
@@ -1,33 +1,40 @@
 import React ,{useEffect, useState} from 'react';
-
+import Mirodposht from '../public/images/Mirodposht.jpg';
+// ==============================================
 
 function Layouttailwind() {
-  const ThemeMode = ()=> {
-    const[theme, setTheme] = useState('light');
-  }
-  const changeMode = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  }
+  const[theme, setTheme] = useState('light');
+
   useEffect(()=> {
     if(theme==='dark') {
-      document.querySelector('html')?.classList.add('dark');
+      document.documentElement.classList.add("dark");
     }else{
-      document.querySelector('html')?.classList.remove('dark');
-
+      document.documentElement.classList.remove("dark");
     }
   } , [theme]);
+
+  const changeMode = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+  // ==========================================
   return (
-    <>
+    <div className=' h-screen dark:bg-black'
+     style={{backgroundImage:`url(${Mirodposht})`}}>
     <div className='flex justify-end mx-10 my-5'>
       <button onClick={changeMode}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke=" #e0990b" stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="2"> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg> 
+        {theme==="dark" ?
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="yellow" stroke="#00FF00 " stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="1" strok> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg> 
+            :
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke=" #000080" stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="1"> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg> 
+        }
+      
       </button>
     </div>
     <div className='flex flex-col justify-center items-center '>
-      <h1 className='text-3xl text-blue-500 m-10'>Hi there !</h1>
-      <button className='border-4 rounded-sm border-red-400 p-5'>About Me</button>
+      <h1 className='text-3xl text-blue-500 dark:text-green-200 m-10'>Hi there !</h1>
+      <button className='border-4 rounded-sm border-red-400 p-5 dark:text-red-100'>About Me</button>
     </div>
-    </>
+    </div>
   )
 }
 
