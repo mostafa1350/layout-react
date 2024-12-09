2aeab57c745471b3611593f8c9adea55cba57532
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-09 22:43:08 +0330

    use class btn in index.css
    use class btn in index.css by apply keyword

--- layout-react-prj/src/Layouttailwind.jsx
+++ layout-react-prj/src/Layouttailwind.jsx
@@ -3,15 +3,15 @@ import Mirodposht from '../public/images/Mirodposht.jpg';
 // ==============================================
 
 function Layouttailwind() {
-  const[theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('light');
 
-  useEffect(()=> {
-    if(theme==='dark') {
+  useEffect(() => {
+    if (theme === 'dark') {
       document.documentElement.classList.add("dark");
-    }else{
+    } else {
       document.documentElement.classList.remove("dark");
     }
-  } , [theme]);
+  }, [theme]);
 
   const changeMode = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
@@ -21,20 +21,29 @@ function Layouttailwind() {
     // we can use background image by tailwindConfig.config.js in the "theme" section
     // or we can use this format => bg-[url("../public/images/Mirodposht-dark.jpg")]
     <div className='bg-no-repeat m-0 p-0 bg-cover bg-center bg-mirodposht h-screen dark:bg-black dark:bg-mirodposhtDark'>
-    <div className='flex justify-end mx-10 my-5'>
-      <button onClick={changeMode}>
-        {theme==="dark" ? 
-         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="yellow" stroke="#00FF00 " stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="1" strok> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg> 
+      <div className='flex justify-end mx-10 my-5'>
+        <button onClick={changeMode}>
+          {theme === "dark" ?
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="yellow" stroke="#00FF00 " stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="1" strok> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg>
             :
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke=" #000080" stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="1"> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg> 
-        }
-      
-      </button>
-    </div>
-    <div className='flex flex-col justify-center items-center '>
-      <h1 className='text-3xl text-blue-500 dark:text-green-200 m-10'>Hi there !</h1>
-      <button className='border-4 rounded-sm border-red-400 p-5 dark:text-red-100'>About Me</button>
-    </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke=" #000080" stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="1"> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg>
+          }
+
+        </button>
+      </div>
+      <div className='flex flex-col justify-center items-center '>
+        <h1 className='text-3xl text-blue-500 dark:text-green-200 m-10'>Hi there !</h1>
+        <button className='border-4 rounded-sm border-red-400 p-5 dark:text-red-100'>About Me</button>
+      </div>
+      {/* ===================================== */}
+      {/* we use component from index.css instead of long style for each components ... */}
+
+      <div className='flex justify-center items-center mt-20'>
+        <button className='btn'>Button Me 1</button>
+        <button className='btn'>Button Me 2</button>
+        <button className='btn'>Button Me 3</button>
+
+      </div>
     </div>
   )
 }
