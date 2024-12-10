668651640323c78ed4f44cfb7c2ba179444a6ad9
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-10 20:02:30 +0330

    update dark mode button

--- layout-react-prj/src/Layouttailwind.jsx
+++ layout-react-prj/src/Layouttailwind.jsx
@@ -23,8 +23,8 @@ function Layouttailwind() {
     // we can use background image by tailwindConfig.config.js in the "theme" section
     // or we can use this format => bg-[url("../public/images/Mirodposht-dark.jpg")]
     <div className='bg-no-repeat m-0 p-0 bg-cover bg-center bg-mirodposht h-screen dark:bg-black dark:bg-mirodposhtDark'>
-      <div className='flex justify-end mx-10 my-5'>
-        <button onClick={changeMode}>
+      <div className='flex justify-end '>
+        <button onClick={changeMode} className='my-6 mx-10'>
           {theme === "dark" ?
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="yellow" stroke="#00FF00 " stroke-linecap="round" stroke-linejoin="round" width="40" height="40" stroke-width="1" strok> <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path> </svg>
             :
